076ab0919143f9be9a4d8a0f97974529a3e9ca6e
deusmemes <maxin29999@gmail.com>
2018-11-10 12:10:05 +0400

    laba 3 complete

--- WindowsShip/src/PanelShip.java
+++ WindowsShip/src/PanelShip.java
@@ -3,13 +3,18 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class PanelShip extends JPanel{
+	
 	public static ITransport ship;
 	public static boolean initialization = false;
+	
+	void setShip(ITransport transport) {
+        ship = transport;
+    }
 	
 	@Override 
 	public void paint(Graphics g) { 
 		super.paint(g); 
-		if(initialization) {
+		if(ship != null) {
 			ship.DrawShip(g);
 		}
 	}
